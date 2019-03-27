c2bbcb752f475c0a9b71755107890d617529088e
zhcluo <zhcluo@gmail.com>
2019-03-27 22:03:00 +0800

    init

---
+++ commonservice-config/src/main/java/info/vpswiki/commonserviceconfig/CommonserviceConfigApplication.java
@@ -0,0 +1,16 @@
+package info.vpswiki.commonserviceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class CommonserviceConfigApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommonserviceConfigApplication.class, args);
+  }
+}
